b72dece2d0c79ee7f28a7e0221d47d0077fdfd55
JangJua <jua0610@naver.com>
2020-04-08 00:20:20 +0900

    add dropdown menu

--- frontend/write-me/src/components/Home/Home.js
+++ frontend/write-me/src/components/Home/Home.js
@@ -6,7 +6,22 @@ class Home extends React.Component {
     render(){
         return (
           <body>
-              <h1> WRITE ME </h1>
+          <div className="topNav">
+                
+          </div>
+            <header>
+                <h1> WRITE ME </h1>
+                <div className="dropdown">
+                    <button className="dropBtn"> 카테고리 </button>
+                    <div className="dropdown-content">
+                        <li> 정치 </li>
+                        <li> 사회 </li>
+                        <li> 경제 </li>
+                        <li> 세계 </li>
+                        <li> IT/과학 </li>
+                    </div>
+                </div>
+            </header>
           </body>
         );
     }
