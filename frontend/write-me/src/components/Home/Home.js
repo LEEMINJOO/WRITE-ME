65062d4d7aa4443b8230f7bea7eb639b62ae2100
JangJua <jua0610@naver.com>
2020-04-14 03:29:38 +0900

    get keywords by category

--- frontend/write-me/src/components/Home/Home.js
+++ frontend/write-me/src/components/Home/Home.js
@@ -1,43 +1,51 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import './Home.scss';
 import '../../reset.css';
+import KeywordToday from "./KeywordToday";
 
 class Home extends React.Component {
     state = {
         cateID: -1,
-        category: ["정치", "사회", "경제", "세계", "IT/과학"]
+        category: ["정치","사회","경제","세계","IT/과학"],
     };
+
     handleClick(id, e) {
         this.setState({cateID: id});
         e.preventDefault();
-        console.log('The link was clicked.', id);
     }
+
     render() {
         const {cateID, category} = this.state;
         return (
-            <body>
-            <div className="topNav">
-            </div>
-            <header>
-                <h1> WRITE ME </h1>
-                <div className="dropdown">
-                    <button className="dropBtn">
-                        {cateID === -1 ?
-                            <span> 카테고리 </span>
-                            :  <span> {category[cateID]} </span>
-                        }
-                    </button>
-                    <div className="dropdown-content">
-                        <li onClick={(e) => this.handleClick(0, e)}> 정치 </li>
-                        <li onClick={(e) => this.handleClick(1, e)}> 사회 </li>
-                        <li onClick={(e) => this.handleClick(2, e)}> 경제 </li>
-                        <li onClick={(e) => this.handleClick(3, e)}> 세계 </li>
-                        <li onClick={(e) => this.handleClick(4, e)}> IT/과학 </li>
-                    </div>
+            <>
+                <title> WRITE ME </title>
+                <body>
+                <div className="topNav">
                 </div>
-            </header>
-            </body>
+                <header>
+                    <h1> WRITE ME </h1>
+                    <div className="dropdown">
+                        <button className="dropBtn">
+                            {cateID === -1 ?
+                                <span> 카테고리 </span>
+                                :  <span> {category[cateID]} </span>
+                            }
+                        </button>
+                        <div className="dropdown-menu">
+                            <li onClick={(e) => this.handleClick(0, e)}> 정치 </li>
+                            <li onClick={(e) => this.handleClick(1, e)}> 사회 </li>
+                            <li onClick={(e) => this.handleClick(2, e)}> 경제 </li>
+                            <li onClick={(e) => this.handleClick(3, e)}> 세계 </li>
+                            <li onClick={(e) => this.handleClick(4, e)}> IT/과학 </li>
+                        </div>
+                    </div>
+                </header>
+                { cateID !== -1 ?
+                    <KeywordToday id={cateID}/>
+                    : <></>
+                }
+                </body>
+            </>
         );
     }
 }
--- frontend/write-me/src/components/Home/HomeIndex.js
+++ frontend/write-me/src/components/Home/HomeIndex.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import Home from './Home'
+import KeywordToday from './KeywordToday';
 
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
+    Route
 } from "react-router-dom";
 
-function HomeIndex() {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" exact={true} component={Home}></Route>
-                <Route path="/keywords/:id" component={Home}></Route>
-            </Switch>
-        </Router>
-    );
-}
+const HomeIndex = ({match}) => (
+    <Router>
+        <Switch>
+            <Route exact path={match.url} component={Home} />
+            <Route path= {`${match.path}/keywords/:category`} component={KeywordToday}/>
+        </Switch>
+    </Router>
+)
 
 export default HomeIndex;
