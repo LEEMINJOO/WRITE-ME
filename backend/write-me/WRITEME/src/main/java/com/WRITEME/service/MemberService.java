1f3a7e477c829e9cec688d283dcfd27178d15218
hajeong815 <62226517+hajeong815@users.noreply.github.com>
2020-05-06 22:21:54 +0900

    Delete MemberService.java

--- backend/write-me/WRITEME/src/main/java/com/WRITEME/service/MemberService.java
+++
@@ -1,8 +0,0 @@
-package com.WRITEME.service;
-
-import com.WRITEME.domain.MemberVO;
-
-public interface MemberService {
-	public void register(MemberVO vo) throws Exception;
-	public MemberVO login(MemberVO vo) throws Exception;
-}
