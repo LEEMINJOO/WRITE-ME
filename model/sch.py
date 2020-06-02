4a3fe973ed2aa52175c7f6dd5fdcec1ec6168273
LEEMINJOO <eminj1197@naver.com>
2020-06-02 15:01:14 +0900

    fix bug
    Signed-off-by: LEEMINJOO <eminj1197@naver.com>

--- model/sch.py
+++ model/sch.py
@@ -6,8 +6,8 @@ def job():
     os.system("python ./script.py")
 
 schedule.every().day.at('19:00').do(job)
-schedule.every().day.at('7:00').do(job)
+schedule.every().day.at('07:00').do(job)
 
 while 1:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(30)
